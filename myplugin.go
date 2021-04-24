b1cf85f444ff72eaf171774986842bef5d27eb96
Robins Gupta <robinskumar73@gmail.com>
2021-04-24 18:20:49 +0530

    Plugin modified.

--- myplugin.go
+++ myplugin.go
@@ -2,24 +2,46 @@ package main
 
 import (
 	"github.com/rwynn/monstache/monstachemap"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 // a plugin to convert document values to uppercase
 func Map(input *monstachemap.MapperPluginInput) (output *monstachemap.MapperPluginOutput, err error) {
     doc := input.Document
-    // for k, v := range doc {
-    //     switch v.(type) {
-    //     case string:
-    //         doc[k] = strings.ToUpper(v.(string))
-    //     }
-    // }
 		if payload, ok := doc["payload"]; ok {
 			payload := payload.(map[string]interface{})
 			// there's an element
 			delete(payload, "cells");
+      delete(payload, "descriptionInJSON");
+      delete(payload, "descriptionInMD");
+      
+      if workspaceId, ok := payload["workspaceId"]; ok {
+        switch workspaceId.(type){
+          case string:
+            break;
+          case primitive.Undefined:
+            delete(payload, "workspaceId");
+          default:
+            delete(payload, "workspaceId");
+        }
+      }
 		}
 
 		
     output = &monstachemap.MapperPluginOutput{Document: doc}
     return
+}
+
+
+func Filter(input *monstachemap.MapperPluginInput) (keep bool, err error) {
+  doc := input.Document
+  BLOCK_MODEL_ID := "5UDGkdvYv"
+  // Only keep document with model id of block.
+  if payload, ok := doc["payload"]; ok {
+    payload := payload.(map[string]interface{})
+    if modelId, ok := payload["modelId"].(string); ok {
+      keep =  modelId == BLOCK_MODEL_ID;
+    }
+  }
+  return
 }
